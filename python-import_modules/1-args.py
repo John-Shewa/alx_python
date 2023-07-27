c236a37b9b637c11cb48d1ef82f2b3ad401ef04c
John-Shewa <eldi2002@gmail.com>
2023-07-27 00:53:38 -0700

    dynamic script

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-__name__ == '__main__'
+def argument():
+    argument = input()
+    arg_list = argument.split()
+    lst_len = len(arg_list)
+    if lst_len == 0:
+        print("{} arguments.".format(lst_len))
+    elif lst_len == 1:
+        print("{} argument:".format(lst_len))
+    else:
+        print("{} arguments:".format(lst_len))
+    for i in range(lst_len):
+        print("{}: {}".format(i+1, arg_list[i]))
 
-argument = input()
-arg_list = argument.split()
-lst_len = len(arg_list)
-if lst_len == 0:
-    print("{} arguments.".format(lst_len))
-elif lst_len == 1:
-    print("{} argument:".format(lst_len))
-else:
-    print("{} arguments:".format(lst_len))
-for i in range(lst_len):
-    print("{}: {}".format(i+1, arg_list[i]))
+if __name__ == '__main__':
+    argument()
