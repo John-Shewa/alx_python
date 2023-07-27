500132e06c70e13604bf788ab0bc45a1dcb80980
John-Shewa <eldi2002@gmail.com>
2023-07-27 04:53:11 -0700

    exception

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,8 +3,15 @@ def safe_print_division(a, b):
     """prints the result of division of 2 integers"""
     result = 0
     try:
-        result = a / b        
-    except ZeroDivisionError:
-        return None
-    finally:
+        result = a / b
         print("Inside result: {}".format(result))
+    except ZeroDivisionError:
+        print("Inside result: {}".format(None))
+        result = None 
+    finally:
+        return result
+        
+        
+
+if __name__ == '__main__':
+    safe_print_division()
