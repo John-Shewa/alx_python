7c095a0d21ac7ef6453192c771743976efc16053
John-Shewa <eldi2002@gmail.com>
2023-08-09 21:15:50 +0300

    oop

---
+++ python-classes/3-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module defines a class called Square.
+
+A Square object has a size attribute that represents its side length.
+
+The Square class has one method:
+
+    * __init__(self, size): This method initializes the Square object with the given size.
+
+"""
+
+
+class Square:
+    """ This is a class that defines a square."""
+
+    def __init__(self, size=0):
+        """ Initializes the Square object with the given size."""
+        self.__size = size
+
+        @property
+        
+        def size(self):
+            """The property of size."""
+            return self.__size
+        
+        @size.setter
+        def size(self, value)
+            """A setter for value"""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    def area(self):
+        """ 
+        Args:
+            self.__size: size of the square.
+        Returns:
+            The area of the square.
+        """
+        return (self.__size * self.__size)
