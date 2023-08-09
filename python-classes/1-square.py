a779517652c96516762eb4986d440f6fcb1856dd
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-09 15:37:13 +0300

    oop

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 class Square:
-    """ This is a class that defines a square. 
-
-    Args:
-       size: the size of the square
+    """ 
+    This is a class that defines a square. 
+   
     """
 
     def __init__(self, size=0):
+        """
+        Args:
+            size: the size of the square
+
+        """
         self.__size = size
 
         if not isinstance(size, int):
