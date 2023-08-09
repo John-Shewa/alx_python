233e48fcdad5c85f8d0d4c131b0857aebdaa5d15
John-Shewa <eldi2002@gmail.com>
2023-08-09 22:09:28 +0300

    oop

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -21,16 +21,16 @@ class Square:
         
         def size(self):
             """The property of size."""
-            return self._size
+            return self.__size
         
         @size.setter
         def size(self, size):
             """A setter for value"""
             if not isinstance(size, int):
                 raise TypeError("size must be an integer")
-            elif value < 0:
+            elif size < 0:
                 raise ValueError("size must be >= 0")
-            self._size = size
+            self.__size = size
 
     def area(self):
         """ 
