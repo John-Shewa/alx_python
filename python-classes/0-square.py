2ad009de57d18d90aa856bce22fd8370cfb6c71d
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-09 16:01:36 +0300

    oop

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-class Square:
-    """ This is a class that defines a square. 
 
-    """
+"""This module defines a class called Square.
+
+A Square object has a size attribute that represents its side length.
+
+The Square class has one method:
+
+    * __init__(self, size): This method initializes the Square object with the given size.
+
+"""
+
+
+class Square:
+    """ This is a class that represents a square."""
 
     def __init__(self, size):
-        """
-
-        Args:
-            size: the size of the square
-
-        Returns:
-            the size of a square.
-        """
+        """ Initializes the Square object with the given size."""
         self.__size = size
