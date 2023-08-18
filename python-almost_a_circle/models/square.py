e81a254dcc6fbfbdfeb96d6f59bbaa040458904c
John-Shewa <eldi2002@gmail.com>
2023-08-18 23:13:44 +0300

    python

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,7 +4,7 @@ from models.rectangle import Rectangle
 
 class Square:
     def __init__(self, size, x=0, y=0, id=None):
-         super().__init__(id)
+         super().__init__(size, size, x, y, id)
          self.width = size
          self.height = size
          self.x = x
