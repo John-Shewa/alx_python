0066f9a520e7fad56bd95c41131990543f4cd599
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-18 11:27:32 +0300

    python

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,7 +13,7 @@ class Base:
     def __init__(self, id=None):
         """ initializes the Base object with the given id"""
         if id is not None:
-            self.id = id
+            self.id = id + 1
         else:
             self.__nb_objects += 1
             self.id = self.__nb_objects
