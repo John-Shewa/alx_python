3a80151fdb77335d9854d4460a91e755b4f5e439
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-29 09:21:10 +0300

    ORM

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,10 +4,10 @@ if __name__ == '__main__':
     import sys
     import MySQLdb
 
-    db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                    user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connect = MySQLdb.connect(host="localhost", port=3306,
+                              user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db_connection.cursor()
+    cur = connect.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     result = cur.fetchall()
 
@@ -15,4 +15,4 @@ if __name__ == '__main__':
         print(row)
 
     cur.close()
-    db_connection.close()
+    connect.close()
