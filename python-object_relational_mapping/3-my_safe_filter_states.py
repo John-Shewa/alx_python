44c95b85e6513fbf146f58355577c512d6f00c75
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-29 15:35:27 +0300

    ORM

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,22 +2,22 @@
 """ This is a script that takes in an argument
 and displays all values in the states table of
 where name matches the argument."""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-import sys
-import MySQLdb
+    connect = MySQLdb.connect(host="localhost",
+                              port=3306,
+                              user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3])
+    cur = connect.cursor()
+    cur.execute(
+        """SELECT * FROM states WHERE `name` = %(user)s 
+        ORDER BY states.id ASC""", {'user': user})
 
-__name__ = '__main__'
-connect = MySQLdb.connect(host="localhost",
-                          port=3306,
-                          user=sys.argv[1],
-                          passwd=sys.argv[2],
-                          db=sys.argv[3])
-cur = connect.cursor()
-cur.execute(
-    """SELECT * FROM states WHERE `name` = %(user)s ORDER BY states.id ASC""", {'user': user})
+    for row in cur.fetchall():
+        print(row)
 
-for row in cur.fetchall():
-    print(row)
-
-cur.close()
-connect.close()
+    cur.close()
+    connect.close()
