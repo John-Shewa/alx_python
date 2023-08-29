cd1785a98ee4095655fbc6bc9764f7a81a08cea4
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-29 10:11:20 +0300

    ORM

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ This is a script that prints all 
     states with a name starting with N
-    from the database hbtn_0e_0_usa
-"""
+    from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
