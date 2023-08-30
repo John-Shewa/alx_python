667e892af2d4ba5898d8c6feed556e57c10d09bc
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-30 14:43:52 +0300

    ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -20,7 +20,7 @@ class State(Base):
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primay_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 
