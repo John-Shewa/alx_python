1ed6b5f4071888e658466c93676a6cd26cefdeac
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-30 14:07:44 +0300

    ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """ This file contains the class definition of 
 "state" and an instance "Base" """
-import sqlalchemy
+import sys
+from sqlalchemy import (create_engine)
 from sqlalchemy.ext.decorative import declarative_base
-
 Base = declarative_base()
 
 
 class State(Base):
-    """ This state class inherits from the Base class"""
+    """ This state class inherits from the Base class
+
+        Args:
+            Base (imported class): imported class 
+    """
     __tablename__ = 'states'
 
-    id = sqlalchemy.Column(
-        sqlalchemy.Integer, primay_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primay_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
 
 
