70e5ba03dd963beb77dedc1f6a5c3b2f4edebbc7
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-30 14:30:07 +0300

    ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This file contains the class definition of state"""
+
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.decorative import declarative_base
@@ -7,10 +8,15 @@ Base = declarative_base()
 
 
 class State(Base):
-    """ This class represents a state in a mysql database.
-        Attributes:
-            id: An integer that is used as the primary keyfor the table.
-            name: A string that represents the name of the state. """
+    """ 
+    This class represents a state in a mysql database.
+
+    Attributes:
+        id: An integer that is used as the primary keyfor the table.
+        name: A string that represents the name of the state.
+
+    Args:
+        Base(imported class): [imported class] """
 
     __tablename__ = 'states'
 
@@ -18,5 +24,5 @@ class State(Base):
     name = Column(String(128), nullable=False)
 
 
-engine = sqlalchemy.create_engine('mysql+pymysql://root@localhost:3306/test')
+engine = create_engine('mysql+mysqldb://root@localhost:3306/test')
 Base.metadata.create_all(engine)
