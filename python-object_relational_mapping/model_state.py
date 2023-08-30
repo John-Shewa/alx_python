4647500bc4353103d38bdc234c531dd6a820a6d9
Yohannes Shewakena <yohannessh@dbe.com>
2023-08-30 14:43:04 +0300

    ORM

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.decorative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
