2f1924fb2abf03f11a0dd054e9f2817441b6184a
Yohannes Shewakena <yohannessh@dbe.com>
2023-09-06 13:37:41 +0300

    webframework

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -37,13 +37,12 @@ def python(text):
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ A function that returns an int "n" 
     followed by text "is a number ".
     """
-    if n is int:
-        return "{} is a number".format(n)
+    return ("{} is a number".format(n))
 
 
 if __name__ == "__main__":
