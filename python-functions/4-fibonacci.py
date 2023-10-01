632a37fcf733dfc4eebbbf4ec715ba56a21c2fd0
John-Shewa <eldi2002@gmail.com>
2023-10-01 21:34:21 +0300

    functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -3,7 +3,7 @@
 def fibonacci_sequence(n):
     if n < 0:
         return[]
-    fib_seqence = [0, 1]
-    for i in range(2, n+1):
-        fib_seqence.append(fib_seqence[i - 1] + fib_seqence[i -2])
-    return(fib_seqence)
+    fib_sequence = [0, 1]
+    for i in range(2, n + 1):
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i -2])
+    return(fib_sequence)
