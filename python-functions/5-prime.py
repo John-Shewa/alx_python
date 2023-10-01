949c687fdc99d54c494a1db6871c52c68a2d2439
John-Shewa <eldi2002@gmail.com>
2023-10-01 22:21:31 +0300

    functions

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def is_prime(number):
-    if number%1 == 0 and number%number == 0:
+    if number%1 == 0 and number%number == 0 and number%2 != 0 :
         return True;
     else:
         return False;
