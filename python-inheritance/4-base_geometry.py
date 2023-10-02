5905a91f3571aeb45de85164f808ede84a47a917
Yohannes Shewakena <yohannessh@dbe.com>
2023-10-02 16:33:11 +0300

    inheritance

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A class called Base Geometry"""
+
+
+class BaseGeometry:
+    """ An class with a public instance method
+    def area(self)"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
