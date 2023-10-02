ceb5ebb11117f39a90333ee4f7cde5c9c63a9031
Yohannes Shewakena <yohannessh@dbe.com>
2023-10-02 14:36:11 +0300

    data

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    key = str(key)
-    updated_dict = {}
-    for element in a_dictionary:
-        if key in a_dictionary:
-            updated_dict(a_dictionary, value)
-        else:
-            updated_dict(a_dictionary, key, value)
-    return updated_dict
+
+    if not isinstance(key, str):
+        raise TypeError("key must be a string")
+    a_dictionary[key] = value
