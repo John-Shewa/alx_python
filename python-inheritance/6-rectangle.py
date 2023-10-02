757a235f242d5b9367dab307d60d6178dcd2b2e8
Yohannes Shewakena <yohannessh@dbe.com>
2023-10-02 18:05:27 +0300

    inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -5,13 +5,16 @@ the class BaseGeometry defined in 5-base_geometry.py"""
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
-if __name__ == "__main__":
-    class Rectangle(BaseGeometry):
-        """ A class that defines a rectangle"""
+class Rectangle(BaseGeometry):
+    """ A class that defines a rectangle"""
 
-        def __init__(self, width, height):
-            """ initialization of rectangle"""
-            self.integer_validator("width", width)
-            self.__width = width
-            self.integer_validator("height", height)
-            self.__height = height
+    def __init__(self, width, height):
+        """ initialization of rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+
+if __name__ == "__main__":
+    Rectangle
