0617cd5b820ab6b716435d6d0b7320dc4b814cec
John-Shewa <eldi2002@gmail.com>
2023-10-04 21:45:18 +0300

    network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     if response.status_code == 200:
         print('Body response:')
-        print('\t - type: {}'.format(type(url)))
+        print('\t - type: {}'.format(type(response.text)))
         print('\t - content: {}'.format(response.text))
