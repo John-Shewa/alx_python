1dc48a0a1b26d8260ca9d90f2e0feab9d9e67f6f
John-Shewa <eldi2002@gmail.com>
2023-10-04 21:35:46 +0300

    network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ A script that fetches the url https://alu-intranet.hbtn.io/status"""
 if __name__ == "__main__":
-    from urllib.request import urlopen
+    import requests
 
-    with urlopen("https://alu-intranet.hbtn.io/status") as response:
-        html = response.read()
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+    if response.status_code == 200:
         print('Body response:')
         print('\t - type: {}'.format(type(html)))
         print('\t - content: {}'.format(html))
