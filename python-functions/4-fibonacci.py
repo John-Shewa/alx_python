ef6aa38e1acf1d5f484b38e282c60c56ce832d2b
Yohannes Shewakena <yohannessh@dbe.com>
2023-12-25 12:04:15 +0300

    fucntions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def fibonacci_sequence(n):
-    if n < 0:
+    if n <= 0:
         return []
-    if n == 0 or n == 1:
-        return [0] * n
-    
+    if n == 1:
+        return [0]
+
     fib_sequence = [0, 1]
     for i in range(2, n + 1):
-        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i -2])
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
     return fib_sequence
